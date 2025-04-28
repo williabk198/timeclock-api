9c9b5f487630749803febda4c4d8bb5d3c6546fc
Brandon Williams <brandonkw27@gmail.com>
2025-04-28 11:52:32 -0400

    Added tests and functionality for generic HTTP endpoint handler

---
+++ pkg/errors/doc.go
@@ -0,0 +1,2 @@
+// package errors holds the errors that could be thrown by other packages in this project
+package errors
---
+++ pkg/httputil/doc.go
@@ -0,0 +1,2 @@
+// httputil holds utility types and functions that assist with handling HTTP requests
+package httputil
