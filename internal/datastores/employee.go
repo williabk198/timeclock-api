bdfb888f1a57566e7aa24c2027732918e5638bdd
Brandon Williams <brandonkw27@gmail.com>
2025-11-27 15:46:11 -0500

    implemented employee datastore

--- internal/datastores/employee.go
+++ internal/datastores/employee.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/williabk198/jagsqlb"
+	"github.com/williabk198/jagsqlb/condition"
 	"github.com/williabk198/timeclock/internal/models"
 )
 
@@ -20,30 +21,105 @@ type employeeSqlStore struct {
 }
 
 // Add implements EmployeeDatastore.
-func (e employeeSqlStore) Add(ctx context.Context, item models.Employee) (id uuid.UUID, err error) {
-	panic("unimplemented")
+func (ess employeeSqlStore) Add(ctx context.Context, item models.Employee) (id uuid.UUID, err error) {
+	query, params, err := ess.sqlBuilder.Insert(ess.tableName).Data(item).Returning("id").Build()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	row := ess.dbConn.QueryRowContext(ctx, query, params...)
+	if err := row.Scan(&id); err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
 
 // Delete implements EmployeeDatastore.
-func (e employeeSqlStore) Delete(ctx context.Context, id uuid.UUID) (item models.Employee, err error) {
-	panic("unimplemented")
+func (ess employeeSqlStore) Delete(ctx context.Context, id uuid.UUID) (item models.Employee, err error) {
+	query, params, err := ess.sqlBuilder.Delete(ess.tableName).Where(condition.Equals("id", id)).Returning("*").Build()
+	if err != nil {
+		return models.Employee{}, err
+	}
+
+	row := ess.dbConn.QueryRowContext(ctx, query, params...)
+	return ess.employeeFromRow(row)
 }
 
 // GetAllPaginated implements EmployeeDatastore.
-func (e employeeSqlStore) GetAllPaginated(ctx context.Context, offset uint, limit uint) (items []models.Employee, err error) {
-	panic("unimplemented")
+func (ess employeeSqlStore) GetAllPaginated(ctx context.Context, offset uint, limit uint) (items []models.Employee, err error) {
+	query, params, err := ess.sqlBuilder.Select(ess.tableName, "*").Offset(offset).Limit(limit).Build()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := ess.dbConn.QueryContext(ctx, query, params...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return ess.employeesFromRows(rows)
 }
 
 // GetSpecific implements EmployeeDatastore.
-func (e employeeSqlStore) GetSpecific(ctx context.Context, id uuid.UUID) (item models.Employee, err error) {
-	panic("unimplemented")
+func (ess employeeSqlStore) GetSpecific(ctx context.Context, id uuid.UUID) (item models.Employee, err error) {
+	query, params, err := ess.sqlBuilder.Select(ess.tableName, "*").Where(condition.Equals("id", id)).Build()
+	if err != nil {
+		return models.Employee{}, err
+	}
+	row := ess.dbConn.QueryRowContext(ctx, query, params...)
+	return ess.employeeFromRow(row)
 }
 
 // Update implements EmployeeDatastore.
-func (e employeeSqlStore) Update(ctx context.Context, id uuid.UUID, item models.Employee) (err error) {
-	panic("unimplemented")
+func (ess employeeSqlStore) Update(ctx context.Context, id uuid.UUID, item models.Employee) (err error) {
+	query, params, err := ess.sqlBuilder.Update(ess.tableName).SetStruct(item).Where(condition.Equals("id", id)).Build()
+	if err != nil {
+		return err
+	}
+
+	_, err = ess.dbConn.ExecContext(ctx, query, params...)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ess employeeSqlStore) employeeFromRow(row *sql.Row) (models.Employee, error) {
+	var item models.Employee
+	if err := row.Scan(
+		&item.ID, &item.PersonID, &item.ReportsToID, &item.Title,
+	); err != nil {
+		return item, err
+	}
+	return item, nil
+}
+
+func (ess employeeSqlStore) employeesFromRows(rows *sql.Rows) ([]models.Employee, error) {
+	result := make([]models.Employee, 0)
+	for rows.Next() {
+		var item models.Employee
+		if err := rows.Scan(
+			&item.ID, &item.PersonID, &item.ReportsToID, &item.Title,
+		); err != nil {
+			return nil, err
+		}
+		result = append(result, item)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
 
 func NewEmployeeStore(dbConn *sql.DB) EmployeeDatastore {
-	panic("unimplemented")
+	return employeeSqlStore{
+		dbConn:     dbConn,
+		sqlBuilder: jagsqlb.NewSqlBuilder(),
+		tableName:  "employees",
+	}
 }
